1bcb1c845a42878f1f45e2b7305779d96ab331ee
David Grellscheid <david.grellscheid@uib.no>
2020-11-24 12:01:41 +0100

    Make the two unit test scripts consistent

--- build/make-denoptim_and_unit_test.sh
+++ build/make-denoptim_and_unit_test.sh
@@ -10,4 +10,4 @@ java -jar ../test/junit/junit-platform-console-standalone-1.5.2.jar -cp lib/DENO
 
 
 # To run a specific test
-#java -jar ../test/junit/junit-platform-console-standalone-1.5.2.jar -cp lib/DENOPTIM.jar:lib/cdk-1.4.19.jar:lib/gson-2.8.6.jar  -c denoptim.fragspace.FragmentSpaceTest
+#java -jar ../test/junit/junit-platform-console-standalone-1.5.2.jar -cp lib/DENOPTIM.jar:lib/cdk-1.4.19.jar:lib/gson-2.8.6.jar  -c denoptim.molecule.DENOPTIMVertexTest
